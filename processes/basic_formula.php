95f523282062a64733ed88e9854dac8653477073
alexandrecuer <alexandre.cuer@cerema.fr>
2020-04-18 19:44:58 +0200

    float or int in formula
    upgrade......

--- processes/basic_formula.php
+++ processes/basic_formula.php
@@ -2,11 +2,16 @@
 
 function basic_formula($dir,$processitem)
 {
+
+    // regular expression to recognize a float or int value
+    // use of ?: not to perturb things in creating useless references
+    $Xnbr="(?:[0-9]+\.[0-9]+|[0-9]+)";
+
     //retrieving the formula
     $formula=$processitem->formula;
     $formula=str_replace('\\','',$formula);
     //print("$formula \n");
-    
+
     //checking the output feed
     $out=$processitem->output;
     if(!$out_meta = getmeta($dir,$out)) return false;
@@ -14,23 +19,24 @@ function basic_formula($dir,$processitem)
         echo "ERROR: could not open $dir $out.dat\n";
         return false;
     }
-    
+
     $pieces=preg_split("@(?=(-|\+))@",$formula);
     //$elements > a third dimensionnal array > datas are on level 2
     //for a given level 1, we operate multiplication and division within corresponding level 2 elements and then give a sign to the result
     //the final result is the addition of the whole
-    //each subarray level 2 is constructed as follow 
+    //each subarray level 2 is constructed as follow
     //[0]=> "feed" or "value"
     //[1]=> operator +,-,* or /
-    //[2]=> if feed multiplicator value (integer), if value : value number (integer)
+    //[2]=> if feed multiplicator value (int/float), if value : value number (int/float)
     //[3]=> does not exist for value, if feed : feed number
+    // nota : feed multiplicator value is filled only when user forgot to use operator *
     $elements=[];
     foreach($pieces as $piece){
         $temp=preg_split("@(?=(\*|\/))@",$piece);
         $fly=[];
         $i=0;
         foreach($temp as $t){
-            if ($result=preg_match("/(-|\+|\*|\/)?(\d+)?(f\d+)?/",$t,$b)){
+            if ($result=preg_match("/(-|\+|\*|\/)?($Xnbr)?(f\d+)?/",$t,$b)){
                 //if we've got 4 elements, we face a feed
                 //if not we face a value
                 if(sizeof($b)==4) {
@@ -55,14 +61,14 @@ function basic_formula($dir,$processitem)
     //print_r($elements);
     $feeds_meta=[];
     $feeds_dat=[];
-    
+
     //we catch the distinct feed numbers involved in the formula
     $feed_ids=[];
     while(preg_match("/(f\d+)/",$formula,$b)){
         $feed_ids[]=substr($b[0],1,strlen($b[0])-1);
         $formula=str_replace($b[0],"",$formula);
     }
-    
+
     //we retrieve the meta and open the dat files
     foreach ($feed_ids as $id){
         if(!$meta = getmeta($dir,$id)) return false;
@@ -73,9 +79,9 @@ function basic_formula($dir,$processitem)
         }
         $feeds_dat[$id]=$fh;
     }
-    
+
     $compute_meta= call_user_func_array("compute_meta",$feeds_meta);
-    
+
     //reading the output meta and if dat file is empty, we adjust interval and start_time
     //we do not report the values in the meta file at this stage. we wait for the dat file to be filled with processed datas
     //if dat file is not empty, meta file should already contain correct values
@@ -85,17 +91,17 @@ function basic_formula($dir,$processitem)
         $out_meta->start_time=$compute_meta->start_time;
     }
     print("NOTICE : ouput is now : ($out_meta->npoints,$out_meta->interval,$out_meta->start_time) \n");
-    
+
     $writing_start_time=$out_meta->start_time+($out_meta->interval*$out_meta->npoints);
     $writing_end_time=$compute_meta->writing_end_time;
     $interval=$out_meta->interval;
-    
+
     $buffer="";
-    
+
     for ($time=$writing_start_time;$time<$writing_end_time;$time+=$interval){
         $s=[];
         foreach($elements as $element){
-            $values=[]; 
+            $values=[];
             foreach($element as $e){
                 $value=NAN;
                 if ($e[0]=="feed"){
@@ -120,16 +126,16 @@ function basic_formula($dir,$processitem)
         if (!in_array(NAN,$s)){
             $sum=array_sum($s);
         } else $sum=NAN;
-                
+
         //print_r("$sum \n");
         $buffer.=pack("f",$sum);
     }
-    
+
     if(!$buffer) {
         print("WARNING: nothing to write - all is up to date \n");
         return false;
     }
-    
+
     if(!$written_bytes=fwrite($out_fh,$buffer)){
         print("ERROR: unable to write to the file with id=$out \n");
         foreach ($feeds_dat as $f) fclose($f);
@@ -145,7 +151,7 @@ function basic_formula($dir,$processitem)
     print("last time value: $time / $sum \n");
     updatetimevalue($out,$time,$sum);
     return true;
-    
+
 
 }
 ?>
