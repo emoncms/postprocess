f5e86cd21de695a0e776340f5c5defa9905bf398
TrystanLea <trystan.lea@gmail.com>
2023-06-19 13:12:47 +0100

    update offset and scale

--- processes/scalefeed.php
+++ processes/scalefeed.php
@@ -15,71 +15,81 @@ class PostProcess_scalefeed extends PostProcess_common
         );
     }
 
-    public function process($processitem)
+    public function process($params)
     {
-        $result = $this->validate($processitem);
+        $result = $this->validate($params);
         if (!$result["success"]) return $result;
-        
-        $dir = $this->dir;
-        $input = $processitem->input;
-        $scale = $processitem->scale;
-        $output = $processitem->output;
 
-        $input_meta = getmeta($dir,$input);
-        
-        createmeta($dir,$output,$input_meta);
-        $output_meta = getmeta($dir,$output);
-        // if ($om->npoints >= $im->npoints) {
-        //   print "output feed already up to date\n";
-        //   return false;
-        // }
+        $input_meta = getmeta($this->dir,$params->input);
+        $output_meta = getmeta($this->dir,$params->output);
 
-        if (!$input_fh = @fopen($dir.$input.".dat", 'rb')) {
-            echo "ERROR: could not open $dir $input.dat\n";
-            return false;
+        // Check that output feed is empty or has same start time and interval
+        if ($output_meta->npoints>0) {
+            if ($input_meta->start_time != $output_meta->start_time) {
+                return array("success"=>false, "message"=>"start time mismatch");
+            }
+            if ($input_meta->interval != $output_meta->interval) {
+                return array("success"=>false, "message"=>"interval mismatch");
+            }
+        } else {
+            // Copies over start_time to output meta file
+            createmeta($this->dir,$params->output,$input_meta);
+        }
+
+        // If recent mode, check that input feed has more points than output feed
+        if ($params->process_mode=='recent' && $output_meta->npoints >= $input_meta->npoints) {
+            return array("success"=>true, "message"=>"output feed already up to date");
         }
         
-        if (!$output_fh = @fopen($dir.$output.".dat", 'c+')) {
-            echo "ERROR: could not open $dir $output.dat\n";
-            return false;
+        if (!$if = @fopen($this->dir.$params->input.".dat", 'rb')) {
+            return array("success"=>false, "message"=>"could not open input feed");
         }
         
-        // get start position
-        $start_pos = $output_meta->npoints;
-        
-        // get end position
-        $end_pos = $input_meta->npoints;
+        if (!$of = @fopen($this->dir.$params->output.".dat", 'c+')) {
+            return array("success"=>false, "message"=>"could not open output feed");
+        }
         
         $buffer = "";
-        $A = 0;
-        $B = 0;
+        $start_pos = 0;
+
+        if ($params->process_mode=='from' && $output_meta->npoints>0) {
+            $start_pos = floor(($params->process_start - $input_meta->start_time) / $input_meta->interval);
+            if ($start_pos<0) $start_pos = 0;
+            if ($start_pos>$input_meta->npoints) {
+                return array("success"=>false, "message"=>"start time is after end of input feed");
+            }
+        }
+
+        if ($params->process_mode=='recent' && $output_meta->npoints>0) {
+            $start_pos = $output_meta->npoints;
+        }
+
+        if ($start_pos>0) {
+            fseek($if,$start_pos*4);
+            fseek($of,$start_pos*4);
+        }
         
-        fseek($input_fh,$start_pos*4);
-        fseek($output_fh,$start_pos*4);
-        
-        for ($n=($start_pos+1); $n<=$end_pos; $n++) {
-            $input_tmp = unpack("f",fread($input_fh,4));
+        for ($n=$start_pos; $n<$input_meta->npoints; $n++) {
+            $tmp = unpack("f",fread($if,4));
             
-            $value = 1*$input_tmp[1];
-            if (!is_nan($input_tmp[1])) { 
-                $value *= $scale;
+            $value = NAN;
+            if (!is_nan($tmp[1])) {
+                $value = 1*$tmp[1]*$params->scale;
             }
             $buffer .= pack("f",$value);
         }
         
-        fwrite($output_fh,$buffer);
+        fwrite($of,$buffer);
         
-        $byteswritten = strlen($buffer);
-        print "bytes written: ".$byteswritten."\n";
-        fclose($output_fh);
-        fclose($input_fh);
+        print "bytes written: ".strlen($buffer)."\n";
+        fclose($of);
+        fclose($if);
         
         $time = $input_meta->start_time + ($input_meta->npoints * $input_meta->interval);
         
-        if ($byteswritten>0) {
-            print "last time value: ".$time." ".$value."\n";
-            updatetimevalue($output,$time,$value);
-        }
+        print "last time value: ".$time." ".$value."\n";
+        updatetimevalue($params->output,$time,$value);
+        
         return array("success"=>true);
     }
 }
