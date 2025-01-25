ed7a7e6a57a7ebef274bd0f54454098b18c471a6
Alexandre CUER <alexandrep.cuer@gmail.com>
2025-01-25 16:09:19 +0100

    remove useless test
    from is not possible, it should be all but it is not needed

--- processes/basic_formula.php
+++ processes/basic_formula.php
@@ -175,7 +175,7 @@ class PostProcess_basic_formula extends PostProcess_common
         //we do not report the values in the meta file at this stage. we wait for the dat file to be filled with processed datas
         //if dat file is not empty, meta file should already contain correct values
         print("NOTICE : ouput is : ($out_meta->npoints,$out_meta->interval,$out_meta->start_time) \n");
-        if($out_meta->npoints==0 || $processitem->process_mode=='from') {
+        if($out_meta->npoints==0) {
             $out_meta->interval=$compute_meta->interval;
             $out_meta->start_time=$compute_meta->start_time;
         }
